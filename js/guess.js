9e966e978a428fa7d7b987fe9e001b05cf4eeb20
istchanar <istchanar@outlook.com>
2021-08-23 05:12:46 +0700

    add_guess3

--- js/guess.js
+++ js/guess.js
@@ -4,6 +4,9 @@ var second = "";
 var firstPlayerInGame = true;
 var secondPlayerInGame = true;
 var turn = 1;
+var tryCountFirst = 0;
+var tryCountSecond = 0;
+var maxTryCount = 2;
   
 //Читает строку из инпута;
 function readInt(){
@@ -29,19 +32,21 @@ function switchTurn(turnNow, playerStatus, message){
     clearInp();
 }
 
+function hide(id){
+    document.getElementById(id).style.display = "none"
+}
+
 function guess(){
     if(first==""){
         first = readInt();
         write("Приятно познакомиться, " + first + "!<br> Второй игрок, введите ваше имя: ");
-        clearInp(); 
-        return;
+        clearInp();
     }
 
     else if (second==""){
         second = readInt();
         write("Приятно познакомиться, " + second + "!<br> Игрок " + first + ", ваш ход!");
-        clearInp(); 
-        return;
+        clearInp();
     }
 
     else {
@@ -49,55 +54,71 @@ function guess(){
         if (firstPlayerInGame == true || secondPlayerInGame == true){
 
             if(turn == 1 && firstPlayerInGame == true){
-
+                tryCountFirst++;
                 var userAnswer = readInt();                 
                 
                 if(parseInt(userAnswer) == answer){
                     switchTurn(2, secondPlayerInGame, "Поздравляю, " + first + " угадал число!<br> Ходит " + second + ":");
-                    return;
                 } 
+                else if(tryCountFirst > maxTryCount)
+                {
+                    if(tryCountSecond>maxTryCount){
+                        switchTurn(2, secondPlayerInGame, "Игрок, " + first + " проиграл(");
+                        hide("userAnswer");
+                    }
+                    else{
+                    switchTurn(2, secondPlayerInGame, "Игрок, " + first + " проиграл(<br> Ходит " + second + ":");
+                    }
+                    firstPlayerInGame = false;
+                    first = second;
+                }
                 else if(parseInt(userAnswer) > answer){
-                    switchTurn(2, secondPlayerInGame, first + " ввёл слишком большое число.<br> Ходит " + second + ":");
-                    return;
+                    switchTurn(2, secondPlayerInGame, first + " ввёл слишком большое число. " + tryCountFirst + " <br> Ходит " + second + ":");
                 } 
                 else if(parseInt(userAnswer) < answer){
-                    switchTurn(2, secondPlayerInGame, first + " ввёл слишком маленькое число.<br> Ходит " + second + ":");
-                    return;
+                    switchTurn(2, secondPlayerInGame, first + " ввёл слишком маленькое число. " + tryCountFirst + "<br> Ходит " + second + ":");
                 } 
                 else if(userAnswer == "выход"){
                     switchTurn(2, secondPlayerInGame, first + " вышел из игры!<br> Ходит " + second + ":");
                     firstPlayerInGame = false;
                     first = second;
-                    return;
                 }
             }
             else if (turn == 2 && secondPlayerInGame == true){
-
+                tryCountSecond++;
                 var userAnswer = readInt();
                 
                 if(parseInt(userAnswer) == answer){
                     switchTurn(1, firstPlayerInGame, "Поздравляю, " + second + " угадал число!<br> Ходит " + first + ":");
-                    return;
                 } 
+                else if(tryCountSecond > maxTryCount)
+                {
+                    if(tryCountFirst>maxTryCount){
+                        switchTurn(2, firstPlayerInGame, "Игрок, " + second + " проиграл(");
+                        hide("userAnswer");
+                    }
+                    else{
+                    switchTurn(2, secondPlayerInGame, "Игрок, " + second + " проиграл(<br> Ходит " + first + ":");
+                    }
+                    secondPlayerInGame = false;
+                    second = first;
+                }
                 else if(parseInt(userAnswer) > answer){
                     switchTurn(1, firstPlayerInGame, second + " ввёл слишком большое число.<br> Ходит " + first + ":");
-                    return;
                 } 
                 else if(parseInt(userAnswer) < answer){
                     switchTurn(1, firstPlayerInGame, second + " ввёл слишком маленькое число.<br> Ходит " + first + ":");
-                    return;
                 } 
                 else if(userAnswer == "выход"){
                     switchTurn(1, firstPlayerInGame, second + " вышел из игры!<br> Ходит " + first + ":");
                     secondPlayerInGame = false;
                     second = first;
-                    return;
                 }
             }
         }
         else{
         write("Все игроки вышли!");
-        return;
+        hide("userAnswer");
         }
     }
 }
