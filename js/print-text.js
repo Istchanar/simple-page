986e272a0e5ee21d443ba86fb424f724faa43763
istchanar <istchanar@outlook.com>
2021-08-26 04:54:18 +0700

    add_print2

--- js/print-text.js
+++ js/print-text.js
@@ -1,13 +1,13 @@
 var someText = "Это эксперементальные страницы для тестирования основ html/css/js.\nВ меню выше размещен ряд ссылок полезных ссылок, плюс ссылки на мини js-игры.\nИсходный код страницы:";
 var getElement = document.getElementById("print");
 
-function write(someText, getElement) {
+function print(someText, getElement) {
     if(someText.length > 0) {
         getElement.innerHTML += someText[0];
         setTimeout(()=> {
-            write(someText.slice(1), getElement, 50);  
+            print(someText.slice(1), getElement);  
         }, 50);
     }
 }
 
-write(someText, getElement);
+print(someText, getElement);
